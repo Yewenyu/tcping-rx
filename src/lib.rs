28c323997d9434a295f9b9e70aae321f7ee145d2
ye <289193866@qq.com>
2023-03-08 16:51:00 +0800

    tcping

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
 mod tcping;
+use tcping::*;
+
+pub fn start_tcp_ping(addrs: &[String],send_byte:bool,timeout:u64,max_count:i32,handle:& dyn Fn(String,i32)->bool){
+    tcp_pings(addrs, send_byte, timeout, max_count, handle);
+}
